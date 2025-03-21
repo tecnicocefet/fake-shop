633353b46d971c48cb49e8bc9422a2816de27861
tecnicocefet <tecnicocefet@gmail.com>
2025-03-21 14:58:38 -0300

    fix 8

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
+
+# Verifica se o banco de dados está pronto
+while ! nc -z postgre 5432; do
+  echo "Aguardando o banco de dados..."
+  sleep 2
+done
+
+# Aplica as migrações
+echo "Aplicando migrações..."
 python -m flask db upgrade
+
+# Inicia a aplicação
+echo "Iniciando a aplicação..."
 python -m gunicorn --bind 0.0.0.0:5000 index:app
--- src/index.py
+++ src/index.py
@@ -47,6 +47,23 @@ def apply_migrations():
         except Exception as e:
             print(f"Error applying migrations: {e}")
 
+def check_db_connection():
+    """Verifica se a aplicação consegue se conectar ao banco de dados."""
+    with app.app_context():
+        try:
+            db.engine.connect()
+            print("Conexão com o banco de dados estabelecida com sucesso!")
+        except Exception as e:
+            print(f"Erro ao conectar ao banco de dados: {e}")
+            raise e
+
+@app.before_first_request
+def initialize_app():
+    """Inicializa a aplicação e aplica as migrações."""
+    check_db_connection()
+    apply_migrations()
+
+# Rotas da aplicação (mantidas como estão)
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
@@ -66,193 +83,7 @@ def checkout_get():
 
     return render_template('checkout.html', items=items, subtotal=subtotal, total=total)
 
-
-@app.route('/checkout', methods=['POST'])
-def checkout():
-    # Obtém dados do formulário de checkout
-    user_name = f"{request.form['first_name']} {request.form['last_name']}"
-    user_email = request.form['email']
-    mobile = request.form['mobile']
-    address1 = request.form['address1']
-    address2 = request.form.get('address2', '')  # Opcional
-    city = request.form['city']
-    state = request.form['state']
-    country = request.form.get('country', 'Brasil')  # Padrão para Brasil
-    zip_code = request.form['zip']
-
-    # Informações de pagamento
-    card_name = request.form['card_name']
-    card_number = request.form['card_number']
-    expiry_date = request.form['expiry_date']
-    cvv = request.form['cvv']
-
-    # Verifica se há um pedido aberto
-    order = Order.query.filter_by(is_open=True).first()
-    if not order:
-        flash("Não há itens no carrinho para finalizar o pedido.", "error")
-        return redirect(url_for('cart'))
-
-    # Atualiza o pedido com dados do usuário e do endereço
-    order.user_name = user_name
-    order.user_email = user_email
-    order.mobile = mobile
-    order.address1 = address1
-    order.address2 = address2
-    order.city = city
-    order.state = state
-    order.country = country
-    order.zip_code = zip_code
-
-    # Atualiza o pedido com informações do cartão de crédito
-    order.card_name = card_name
-    order.card_number = card_number
-    order.expiry_date = expiry_date
-    order.cvv = cvv
-
-    # Gera um número único para o pedido e fecha o pedido
-    order.order_number = generate_order_number()
-    order.is_open = False
-
-    # Salva as alterações no banco de dados
-    db.session.commit()
-
-    # Confirmação de sucesso
-    flash(f"Pedido realizado com sucesso! Número do pedido: {order.order_number}", "success")
-    return redirect(url_for('order_confirmation', order_number=order.order_number))
-
-
-@app.route('/order_confirmation/<order_number>')
-def order_confirmation(order_number):
-    order = Order.query.filter_by(order_number=order_number).first()
-    return render_template('order_confirmation.html', order=order)
-
-@app.route('/shop')
-def shop():
-    products = Product.query.all()
-    return render_template('shop.html', products=products)
-
-def get_or_create_order():
-    uuid_order_id = request.cookies.get('order_id')
-
-    order = None
-    response = None
-
-    if uuid_order_id:
-        order = Order.query.filter(
-            Order.uuid == uuid_order_id, Order.is_open == True
-        ).first()
-
-    if not order:
-        order = Order(uuid=uuid.uuid4(), total_price=0.0, is_open=True)
-        db.session.add(order)
-        db.session.commit()
-
-        response = redirect(request.url)
-        response.set_cookie('order_id', str(order.uuid))  # Convertendo UUID para string
-
-    return order, response
-
-@app.route('/add_to_cart/<int:product_id>', methods=['POST'])
-def add_to_cart(product_id):
-    product = Product.query.get_or_404(product_id)
-    print("Entrou no add_to_cart")
-    order, response = get_or_create_order()
-    quantity = int(request.form.get("quantity"))
-
-    order_item = OrderItem.query.filter_by(order_id=order.id, product_id=product_id).first()
-
-    if order_item:
-        order_item.quantity += quantity
-    else:
-        order_item = OrderItem(
-            order_id=order.id,
-            product_id=product.id,
-            quantity=quantity,
-            price=product.price
-        )
-        db.session.add(order_item)
-
-    db.session.commit()
-
-    flash(f'{product.name} adicionado ao carrinho!', 'success')
-
-    redirect_response = make_response(redirect(url_for('detail', product_id=product.id)))
-
-    if response:
-        for cookie_key, cookie_value in response.headers.items():
-            if cookie_key.startswith("Set-Cookie"):
-                redirect_response.headers.add(cookie_key, cookie_value)
-
-    return redirect_response
-
-@app.route('/detail/<int:product_id>')
-def detail(product_id):
-    product = Product.query.get_or_404(product_id)
-    related_products = Product.query.limit(4).all()
-    return render_template('detail.html', product=product, related_products=related_products)
-
-def get_order_from_cookie():
-    order_id = request.cookies.get('order_id')
-    if not order_id:
-        return None
-
-    try:
-        # Converte o valor do cookie para UUID
-        uuid_order_id = uuid.UUID(order_id)
-    except ValueError:
-        # Retorna None se a conversão falhar
-        return None
-
-    # Ajuste na consulta para converter explicitamente para string
-    return Order.query.filter(
-        Order.uuid == str(uuid_order_id), Order.is_open == True
-    ).first()
-
-
-@app.route('/cart', methods=['GET', 'POST'])
-def cart():
-    order = get_order_from_cookie()
-    if not order:
-        flash('Seu carrinho está vazio.', 'warning')
-        return render_template('cart.html', items=[], subtotal=0, total=0)
-
-    items = order.items
-    subtotal = sum(item.quantity * item.price for item in items)
-    shipping = 10.0
-    total = subtotal + shipping
-
-    return render_template('cart.html', items=items, subtotal=subtotal, total=total)
-
-
-@app.route('/update_quantity/<int:item_id>', methods=['POST'])
-def update_quantity(item_id):
-    new_quantity = int(request.form.get('quantity'))
-    item = OrderItem.query.get_or_404(item_id)
-
-    if new_quantity <= 0:
-        db.session.delete(item)
-    else:
-        item.quantity = new_quantity
-
-    db.session.commit()
-    flash('Quantidade atualizada com sucesso!', 'success')
-    return redirect(url_for('cart'))
-
-
-@app.route('/remove_item/<int:item_id>', methods=['POST'])
-def remove_item(item_id):
-    item = OrderItem.query.get_or_404(item_id)
-    db.session.delete(item)
-    db.session.commit()
-
-    flash('Item removido do carrinho.', 'success')
-    return redirect(url_for('cart'))
-
-
-@app.route('/')
-def index():
-    products = Product.query.all()
-    return render_template('index.html', products=products)
+# ... (restante das rotas)
 
 if __name__ == '__main__':
     #apply_migrations()
